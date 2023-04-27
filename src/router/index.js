88b35b7f53201116770f2a140aaa208fc658d3c2
festsnusa <lukatimof@mail.ru>
2023-04-27 19:18:18 +0700

    added routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CardView from '../views/CardView.vue'
+import NotFound from '../views/NotFound.vue'
+import json from '../data.json'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,13 +13,15 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/:name',
+      name: 'card',
+      component: CardView,
+      props: route => ({ arr: json.filter(e => e.name === route.params.name) })
+    },
+    {
+      path: "/:catchAll(.*)",
+      component: NotFound,
+    },
   ]
 })
 
