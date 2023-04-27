f7604dc05d56575b7e235765fe0ed64287b58670
festsnusa <lukatimof@mail.ru>
2023-04-27 19:19:56 +0700

    methods moved to HomeView

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,19 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
-import AppFilter from './components/AppFilter.vue'
-import AppCard from './components/AppCard.vue'
-import json from './data.json'
 export default {
   name: "App",
   components: {
     AppHeader,
-    AppFilter,
-    AppCard
   },
   data() {
     return {
       mode: 'dark',
-      arr: json,
     }
   },
-  methods: {
-    filter() {
-      this.arr = json
-      console.log(this.arr.length)
-      const region = document.querySelector('.search__select').value
-      console.log(typeof region)
-      this.arr = this.arr.filter(e => e.region === region)
-      console.log(this.arr.length)
-    }
-  }
 }
 </script>
 
 <template lang="pug">
 AppHeader(:mode="mode")
-AppFilter(:filter="filter")
-AppCard(:arr="arr")
+RouterView
 </template>
