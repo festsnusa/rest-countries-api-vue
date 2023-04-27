0e0ab9e204b705e1607e7f3eba51f02bcdce4530
festsnusa <lukatimof@mail.ru>
2023-04-27 19:18:41 +0700

    edited HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,39 @@
+<template lang="pug">
+AppFilter(:filter="filter" :searchCountry="searchCountry")
+AppCard(:arr="arr")
+</template>
+
 <script>
+import AppFilter from '../components/AppFilter.vue'
+import AppCard from '../components/AppCard.vue'
+import json from '../data.json'
+
+export default {
+  name: "HomeView",
+  components: {
+    AppFilter,
+    AppCard
+  },
+  data() {
+    return {
+      mode: 'dark',
+      arr: json,
+    }
+  },
+  methods: {
+    filter() {
+      this.arr = json
+      const region = document.querySelector('.search__select').value
+      this.arr = this.arr.filter(e => e.region === region)
+    },
+    searchCountry(value) {
+      this.arr = json
+      if (value.length == 0) return
+      this.arr = this.arr.filter(e => e.name.toLowerCase().includes(value.toLowerCase()))
+      console.log(this.arr)
+    }
+  }
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="scss" scoped></style>
