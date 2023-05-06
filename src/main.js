b3b313d3d1ed60b10800f621fb337717eb30b948
festsnusa <lukatimof@mail.ru>
2023-05-06 13:53:47 +0700

    added pinia plugin

--- src/main.js
+++ src/main.js
@@ -8,14 +8,18 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faMoon } from '@fortawesome/free-regular-svg-icons'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 
 library.add(faMoon, faSearch)
 
 import './assets/main.scss'
 
 const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedState)
+
 app.component('font-awesome-icon', FontAwesomeIcon)
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
