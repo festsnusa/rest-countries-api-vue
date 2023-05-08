781ef577533d397eb454934050d9716aacaf80a0
festsnusa <lukatimof@mail.ru>
2023-05-08 20:30:28 +0700

    no base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rest-countries-api-vue/',
+  // base: '/rest-countries-api-vue/',
   plugins: [vue()],
   resolve: {
     alias: {
